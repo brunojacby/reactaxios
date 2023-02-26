ae9e15bb438af057ffc0045264302dd8f599f361
Bruno Jacby <brunojck@hotmail.com>
2023-02-26 15:04:23 +0000

    adc folder routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { createBrowserRoute, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRoute([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+      },
+      {
+        path: "/new",
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
